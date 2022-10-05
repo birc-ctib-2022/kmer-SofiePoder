f22ab50f2509069bc1f3f95b7395054a42d767c7
SofiePoder <201905630@post.au.dk>
2022-10-05 17:40:19 +0200

    kmer tjek 1

--- src/kmer.py
+++ src/kmer.py
@@ -1,31 +1,54 @@
 """Computing kmers of a string."""
 
-
+kmer_list = []
 def kmer(x: str, k: int) -> list[str]:
+    for i in range(0,len(x)-(k-1),1):
+        kmers = x[i:i+k]
+        kmer_list.append(kmers)
+    return kmer_list
+    
     """
     Computer all k-mers of x.
 
     >>> kmer('agtagtcg', 3)
     ['agt', 'gta', 'tag', 'agt', 'gtc', 'tcg']
 
-    FIXME: do you want more tests here?
+    >>> kmer('agtagtcg', 4)
+    ['agta', 'gtag', 'tagt', 'agtc', 'gtcg']
     """
     ...
 
-
+k_list = []
 def unique_kmers(x: str, k: int) -> list[str]:
     """
     Computer all unique k-mers of x.
 
-    FIXME: do you want more tests here?
+    >>> unique_kmers('agtagtcg', 3)
+    ['agt', 'gta', 'tag', 'gtc', 'tcg']
     """
+    for i in range(len(x)-(k-1)):
+        kmers = x[i:i+k]
+        if kmers not in k_list:
+            k_list.append(kmers)
+    return k_list
+
     ...
 
-
+dic_kmer = {}
 def count_kmers(x: str, k: int) -> dict[str, int]:
     """
     Computer all k-mers of x and count how often they appear.
 
-    FIXME: do you want more tests here?
+    >>> count_kmers('agtagtcg', 3)
+    {'agt': 2, 'gta': 1, 'tag': 1, 'gtc': 1, 'tcg': 1}
     """
-    ...
+    for i in range(len(x)-(k-1)):
+        kmers = x[i:i+k]
+        if kmers not in dic_kmer:
+            dic_kmer[kmers] = 1
+        else:
+            dic_kmer[kmers] += 1
+            
+    return dic_kmer
+
+...
